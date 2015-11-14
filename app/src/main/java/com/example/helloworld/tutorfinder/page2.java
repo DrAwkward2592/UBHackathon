5bc4d31ca840d66b1dd3450f837d1e0d3c33789f
Aditya Bhavnani <Aditya Bhavnani>
2015-11-14 18:02:37 -0500

    page 2 and 4

--- app/src/main/java/com/example/helloworld/tutorfinder/page2.java
+++ app/src/main/java/com/example/helloworld/tutorfinder/page2.java
@@ -1,42 +1,55 @@
+//student page
 package com.example.helloworld.tutorfinder;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class page0 extends AppCompatActivity {
+public class page2 extends AppCompatActivity {
+TextView t2;
+GPSTracker gps;
 Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page0);
+        setContentView(R.layout.activity_page2);
+        t2=(TextView)findViewById(R.id.t2);
         b1=(Button)findViewById(R.id.b1);
         b2=(Button)findViewById(R.id.b2);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(page0.this, page2.class);//(current_javafile.this,filetoshiftto.class)
-                startActivity(intent);
+                gps=new GPSTracker(page2.this);
+                if(gps.canGetLocation()) {
+                    t2.setText("");
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    t2.setText(String.valueOf(latitude) + " , " + String.valueOf(longitude));
+                    Intent intent = new Intent(page2.this, page4.class);//(current_javafile.this,filetoshiftto.class)
+                    startActivity(intent);
+
+                }else{
+                    gps.showSettingsAlert();
+                }
             }
         });
-        //b2.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-         //   public void onClick(View v) {
-          //      Intent intent = new Intent(page0.this, page3.class);//(current_javafile.this,filetoshiftto.class)
-           //     startActivity(intent);
-           // }
-        //});
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_page0, menu);
+        getMenuInflater().inflate(R.menu.menu_page2, menu);
         return true;
     }
 
@@ -54,4 +67,5 @@ Button b1,b2;
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/helloworld/tutorfinder/page4.java
+++ app/src/main/java/com/example/helloworld/tutorfinder/page4.java
@@ -1,42 +1,22 @@
 package com.example.helloworld.tutorfinder;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class page0 extends AppCompatActivity {
-Button b1,b2;
+public class page4 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page0);
-        b1=(Button)findViewById(R.id.b1);
-        b2=(Button)findViewById(R.id.b2);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(page0.this, page2.class);//(current_javafile.this,filetoshiftto.class)
-                startActivity(intent);
-            }
-        });
-        //b2.setOnClickListener(new View.OnClickListener() {
-         //   @Override
-         //   public void onClick(View v) {
-          //      Intent intent = new Intent(page0.this, page3.class);//(current_javafile.this,filetoshiftto.class)
-           //     startActivity(intent);
-           // }
-        //});
+        setContentView(R.layout.activity_page4);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_page0, menu);
+        getMenuInflater().inflate(R.menu.menu_page4, menu);
         return true;
     }
 
